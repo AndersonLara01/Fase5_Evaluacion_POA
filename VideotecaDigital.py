997cf41edeb5e36615346d4cc9ad7a2d265920d7
Anderson Lara <andersonkaleth10@gmail.com>
2026-05-22 21:07:54 -0500

    Configuración matriz

--- VideotecaDigital.py
+++ VideotecaDigital.py
@@ -2,3 +2,12 @@
 # Grupo: 213022_903
 # Programa: Ingeniería de Sistemas
 # Código Fuente: autoría propia
+
+matriz = { "titulo":["Robot salvaje", "Mortal Kombat", "Matilda", "Coraline", "Godzilla vs Kong", "Enredados"], "añoLanzamiento":["2024", "2021", "1996", "2009", "2021", "2010"],
+"calificacion":[],
+"genero":["Animación", "Acción", "Comedia", "Terror", "Acción", "Animación"]
+}
+
+# def popularReciente():
+#     for i in matriz():
+
