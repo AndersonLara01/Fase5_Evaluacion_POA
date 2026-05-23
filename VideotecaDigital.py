1114a0ca9d701e84fcd72ea4c9e3f492cc8385cd
Anderson Lara <andersonkaleth10@gmail.com>
2026-05-22 22:23:30 -0500

    Avance

--- VideotecaDigital.py
+++ VideotecaDigital.py
@@ -3,11 +3,21 @@
 # Programa: Ingeniería de Sistemas
 # Código Fuente: autoría propia
 
-matriz = { "titulo":["Robot salvaje", "Mortal Kombat", "Matilda", "Coraline", "Godzilla vs Kong", "Enredados"], "añoLanzamiento":["2024", "2021", "1996", "2009", "2021", "2010"],
-"calificacion":[],
+peliculas = { "titulo":["Robot salvaje", "Mortal Kombat", "Matilda", "Coraline", "Godzilla vs Kong", "Enredados"], "añoLanzamiento":["2024", "2021", "1996", "2009", "2021", "2010"],
+"calificacion":[9.9, 7.5, 6.3, 6.4, 8.4, 6.1],
 "genero":["Animación", "Acción", "Comedia", "Terror", "Acción", "Animación"]
 }
 
-# def popularReciente():
-#     for i in matriz():
+print('''=============================================================
+|      Titulo     | Año lanzamiento | Calificación | Género |
+|	          |		    |		   |	    |
+''')
 
+def popularReciente():
+    resultado = []
+    for nombre, puntaje, año in zip(peliculas["titulo"], peliculas["calificacion"], peliculas["añoLanzamiento"]):
+        if puntaje >= 7 and año >= "2010":
+            resultado.append(nombre)
+
+
+popularReciente()
