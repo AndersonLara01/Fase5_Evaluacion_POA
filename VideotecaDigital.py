67359c578831b20c09be867bdd0e4bfd7de82f0f
Anderson Lara <andersonkaleth10@gmail.com>
2026-05-23 20:40:55 -0500

    Menú: matriz ejemplo, matriz vacia y salir

--- VideotecaDigital.py
+++ VideotecaDigital.py
@@ -3,24 +3,65 @@
 # Programa: Ingeniería de Sistemas
 # Código Fuente: autoría propia
 
-peliculas = { "titulo":["Robot salvaje", "Mortal Kombat", "Matilda", "Coraline", "Godzilla vs Kong", "Enredados"], "añoLanzamiento":["2024", "2021", "1996", "2009", "2021", "2010"],
-"calificacion":[9.9, 7.5, 6.3, 6.4, 8.4, 6.1],
-"genero":["Animación", "Acción", "Comedia", "Terror", "Acción", "Animación"]
+def mostrarMenu():
+    print("\n------Menú principal-----")
+    print("1. Matriz ejemplo")
+    print("2. Matriz vacia")
+    print("3. Salir")
+    opcion = int(input("Seleccione una opción(1-3): "))
+    if opcion == 1:
+        matrizEjemplo()
+        popularReciente()
+    elif opcion == 2:
+        matrizVacia()
+    elif opcion == 3:
+        print("Programa finalizado...")
+        exit()
+    else:
+        print("Opción no válida. Intente nuevamente.")
+        mostrarMenu()
+    mostrarMenu()
+
+peliculas = {"titulos":["Robot salvaje", "Mortal Kombat", "Matilda", "Coraline", "Anabelle", "Godzilla vs Kong","Enredados"], "añoLanzamiento":["2024", "2021", "1996", "2009", "2014", "2021", "2010"],
+"calificaciones":[9.9, 7.5, 6.3, 6.4, 7.8, 8.4, 6.1],
+"generos":["Animación", "Acción", "Comedia", "Terror", "Terror", "Acción", "Animación"]
 }
 
-print('''==================================================================
+def matrizEjemplo():
+    print('''==================================================================
 |       Titulo      | Año lanzamiento | Calificación |   Género  |
 ==================================================================''')
 
-for i in zip(peliculas["titulo"], peliculas["añoLanzamiento"], peliculas["calificacion"], peliculas["genero"]):
-    print(f'''| {i[0]:<17} | {i[1]:<15} | {i[2]:<12} | {i[3]:<9} |
+    for i in zip(peliculas["titulos"], peliculas["añoLanzamiento"], peliculas["calificaciones"], peliculas["generos"]):
+        print(f'''| {i[0]:<17} | {i[1]:<15} | {i[2]:<12} | {i[3]:<9} |
+|___________________|_________________|______________|___________|''')
+
+def matrizVacia():
+    espaciosVacios = { "titulos":[], "añoLanzamiento":[], "calificaciones":[], "generos":[] }
+    filas = int(input("Cantidad de titulos que desea ingresar: "))
+    for i in range(filas):
+        titulo = input("Ingrese el título de la película: ")
+        año = input("Ingrese el año de lanzamiento: ")
+        calificacion = float(input("Ingrese la calificación: "))
+        genero = input("Ingrese el género: ")
+        espaciosVacios["titulos"].append(titulo)
+        espaciosVacios["añoLanzamiento"].append(año)
+        espaciosVacios["calificaciones"].append(calificacion)
+        espaciosVacios["generos"].append(genero)
+    print('''==================================================================
+|       Titulo      | Año lanzamiento | Calificación |   Género  |
+==================================================================''')
+    for i in zip(espaciosVacios["titulos"], espaciosVacios["añoLanzamiento"], espaciosVacios["calificaciones"], espaciosVacios["generos"]):
+        print(f'''| {i[0]:<17} | {i[1]:<15} | {i[2]:<12} | {i[3]:<9} |
 |___________________|_________________|______________|___________|''')
 
 def popularReciente():
     resultado = []
-    for nombre, puntaje, año in zip(peliculas["titulo"], peliculas["calificacion"], peliculas["añoLanzamiento"]):
-        if puntaje >= 7 and año >= "2010":
-            resultado.append(nombre)
+    for titulo, calificacion, año in zip(peliculas["titulos"], peliculas["calificaciones"], peliculas["añoLanzamiento"]):
+        if calificacion >= 7 and año >= "2020":
+            resultado.append(titulo)
+    print("\n------Peliculas populares y recientes-----")
+    for i in resultado:
+        print("-", i)         
 
-
-popularReciente()
+mostrarMenu()
